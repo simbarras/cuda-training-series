8f0c41289036ddfe0d7917ff1f91341bc89c78e8
Simon Barras <simon.barras@edu.hefr.ch>
2023-05-31 11:06:48 +0200

    Finish exercise one

--- exercises/hw1/hello.cu
+++ exercises/hw1/hello.cu
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
+#define N 3
+#define M 4
+
 __global__ void hello(){
 
-  printf("Hello from block: %u, thread: %u\n", FIXME);
+  printf("Hello from block: %u, thread: %u\n", blockIdx.x, threadIdx.x);
 }
 
 int main(){
-
-  hello<<<FIXME>>>();
+  hello<<<N, M>>>();
   cudaDeviceSynchronize();
 }
 
